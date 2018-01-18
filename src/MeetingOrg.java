61e0094625b034c8905865bfd3451dba60013f94
Sukhvir S Sidhu <Sukhvirz@gmail.com>
2018-01-17 16:01:59 -0800

    first test commit

---
+++ src/MeetingOrg.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Sukhvir
+ *
+ */
+public class MeetingOrg {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("You are Succeefully fork the project");
+
+	}
+
+}
